5c5e7398b5331fd98c0d2f75633483fa188fa99b
dciangot <dciangot@cern.ch>
2016-10-28 11:46:26 +0200

    getDN

--- src/python/Core/__init__.py
+++ src/python/Core/__init__.py
@@ -3,6 +3,11 @@ import time
 import logging
 import hashlib
 import subprocess
+import getopt
+import pycurl
+import json
+import urllib
+from io import BytesIO
 
 __version__ = '1.0.3'
 
@@ -32,7 +37,32 @@ def execute_command(command):
     return stdout, stderr, rc
 
 # TODO: def getDNFromUserName(username, log, ckey = None, cert = None)
+def getDNFromUserName(username, log, ckey = None, cert = None):
+    """
+    get DN for user from siteDB
+    :param username:
+    :param log:
+    :param ckey:
+    :param cert:
+    :return:
+    """
+    c = pycurl.Curl()
+    # TODO: make url configurable
+    c.setopt(c.URL, 'https://cmsweb.cern.ch/sitedb/data/prod/people?match=%s' % username)
+    c.setopt(pycurl.SSL_VERIFYPEER, False)
+    c.setopt(pycurl.SSLKEY, ckey)
+    c.setopt(pycurl.SSLCERT, cert)
+    c.setopt(pycurl.VERBOSE, 1)
+    e = BytesIO()
+    c.setopt(pycurl.WRITEFUNCTION, e.write)
+    c.perform()
+    # TODO: exception
+    results = json.loads(e.getvalue().decode('UTF-8'))
+
+    return results['dn']
 
 # TODO: def getProxy(defaultDelegation, log)
+
+
 
 # TODO: LOGGING
